63c6375606427cb6e880b9a2cad1894102269131
Arturo Guzman <247952+guzart@users.noreply.github.com>
2018-02-09 18:30:30 -0500

    chore: fix quote author; add quote

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -9,7 +9,7 @@ class RootController < ApplicationController
     # '“” – ',
     [
       '“First, solve the problem. Then, write the code.” – John Johnson',
-      '“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – John Johnson',
+      '“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler',
       '“One of my most productive days was throwing away 1,000 lines of code.” – Ken Thompson',
       '“It is not enough for code to work.” – Robert C. Martin',
       '“A long descriptive name is better than a short enigmatic name. A long descriptive name is better than a long descriptive comment.” – Robert C. Martin',
@@ -17,6 +17,7 @@ class RootController < ApplicationController
       '“Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.” – Linus Torvalds',
       '“Measuring programming progress by lines of code is like measuring aircraft building progress by weight.” – Bill Gates',
       '“Good design adds value faster than it adds cost.” – Thomas C. Gale',
+      '“Good code is its own best documentation. As you\'re about to add a comment, ask yourself, "How can I improve the code so that this comment isn\'t needed?" Improve the code and then document it to make it even clearer.” – Steve McConnell',
     ].sample
   end
 end
