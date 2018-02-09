67387a17c00be19b07af651dc8bd728bde28aab9
Arturo Guzman <ing.arturo.guzman@gmail.com>
2018-02-09 01:06:39 -0500

    feat: add authorization to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  NotAuthorizedError = Class.new(StandardError)
+
+  before_action :authorize!
+
+  rescue_from NotAuthorizedError, with: :unauthorized
+
+  private
+
+  def authorize!
+    if request.headers['x-api-key'] != ENV['API_KEY']
+      raise NotAuthorizedError
+    end
+  end
+
+  def unauthorized
+    render status: :unauthorized, json: { message: 'Not authorized' }
+  end
 end
